46339d1d60b14cfe12ee4024ebd50a6c9a976c9b
Cleidson Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2022-04-21 12:49:44 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Bem vindo ao ByteBank")
+
+    var titular : String = "Cleidson Bononi"
+    val numeroConta : Int = 1000
+    var saldo : Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 198
+
+
+
+    println("Titular $titular")
+    println("Numero de Conta $numeroConta")
+    println ("Saldo da Conta $saldo")
+
+}
