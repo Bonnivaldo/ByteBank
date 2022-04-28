33b8a2d0d6e898d2076cd7ef6929403f439b4b33
Cleidson Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2022-04-28 10:18:52 -0300

    aulaPolimorfismo
    aulaPolimorfismo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,12 @@
 fun main() {
     println("Bem vindo ao ByteBank")
 
-    val contaAlex = Conta("Alex", 1000)
-    contaAlex.mostraDadosConta()
-
-    val contaFran = Conta("Fran" , 1001)
-    contaFran.mostraDadosConta()
-
-    contaFran.deposita(300.0)
-    contaAlex.deposita(100.0)
-
-    contaFran.deposita(200.0)
-
-    contaFran.saque(300.0)
-
-    contaFran.trasfere(50.0, contaAlex)
-    //teste
-    println(contaAlex.saldo)
-
+    testaComportamentoConta()
 
 
 }
+
+
 
 
 
---
+++ src/main/kotlin/testaComportamentoConta.kt
@@ -0,0 +1,18 @@
+fun testaComportamentoConta() {
+    val contaAlex = Conta("Alex", 1000)
+    contaAlex.mostraDadosConta()
+
+    val contaFran = Conta("Fran", 1001)
+    contaFran.mostraDadosConta()
+
+    contaFran.deposita(300.0)
+    contaAlex.deposita(100.0)
+
+    contaFran.deposita(200.0)
+
+    contaFran.saque(300.0)
+
+    contaFran.trasfere(50.0, contaAlex)
+    //teste
+    println(contaAlex.saldo)
+}
